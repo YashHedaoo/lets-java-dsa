615c72e85ae0c9e56f8a73ea141ae1f9f7c6d4fa
Yash Hedaoo <yashhedaoo@Yashs-MacBook-Air.local>
2025-06-16 23:40:49 +0530

    first-program

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main { 
+    public static void main(String[] args) {
+        Scanner Scanner= new Scanner(System.in);
+        System.out.println("Please enter the number:");
+        long number = Scanner.nextLong();
+
+        if (number%2 == 0) {
+            System.out.println("number is even");
+            } else {
+            System.out.println("number is odd");
+
+            }
+        };
+
+
+    }
+    
