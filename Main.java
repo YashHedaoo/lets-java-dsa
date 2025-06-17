b6b92106cf588dfea8e9ac91aabe0cd63fd001d2
Yash Hedaoo <yashhedaoo@Yashs-MacBook-Air.local>
2025-06-17 15:11:52 +0530

    some assignments

--- Main.java
+++ Main.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public class Main { 
     public static void main(String[] args) {
-        // Scanner Scanner= new Scanner(System.in);
-        // System.out.println("Please enter the number:");
-        // long number = Scanner.nextLong();
+        Scanner Scanner= new Scanner(System.in);
+        System.out.println("Please enter the number:");
+        long number = Scanner.nextLong();
 
-        // if (number%2 == 0) {
-        //     System.out.println("number is even");
-        //     } else {
-        //     System.out.println("number is odd");
+        if (number%2 == 0) {
+            System.out.println("number is even");
+            } else {
+            System.out.println("number is odd");
 
-        //     }
-        // };
+            }
+        };
         
 
     }
